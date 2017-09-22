6326082373c86be705bad83f0619c041ebcc372e
Nathan Wilcox <nathan@z.cash>
2017-09-22 13:28:55 +0900

    First pass / rought draft / untested.

--- cryptonode/main.py
+++ cryptonode/main.py
@@ -1,19 +1,59 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import argparse
+from pathlib2 import Path
+
+
+executable = Path(sys.argv[0])
 
 
 def main(args=sys.argv[1:]):
     """
     Manager multiple cryptocurrency nodes locally.
     """
+    execname = os.path.basename(sys.argv[0])
+    if execname == 'cryptonode':
+        cryptonode_main(args)
+    else:
+        wrapper_main(args)
+
+
+def cryptonode_main(args):
     opts = parse_args(args)
-    raise NotImplementedError('main')
+    bindir = opts.DIR / 'bin'
+    for profdir in (opts.DIR / 'profiles').iterdir():
+        for binfile in (profdir / 'bin').iterdir():
+            if binfile.is_file() and binfile.stat().st_mode & 0100:
+                wrappername = '{}.{}'.format(profdir.name, binfile.name)
+                wrapperdest = bindir / wrappername
+                print('Creating {}'.format(wrapperdest))
+                wrapperdest.symlink_to(executable)
+
+
+def wrapper_main(args):
+    assert executable.is_symlink(), executable
+    [profname, binname] = executable.name.split('.', 1)
+    profdir = executable.parent.parent / 'profiles' / profname
+    datadir = profdir / 'datadir'
+    targetbin = profdir / 'bin' / binname
+    fullargs = [str(targetbin), '-datadir=' + str(datadir)] + args
+    print('cryptoprofile running: {!r}'.format(fullargs))
+    os.execvp(fullargs[0], fullargs)
 
 
 def parse_args(args):
     p = argparse.ArgumentParser(description=main.__doc__)
+
+    dirdefault = os.path.expanduser('~/.cryptonode')
+    p.add_argument(
+        '--dir',
+        dest='DIR',
+        type=Path,
+        default=Path(dirdefault),
+        help='Base config directory. Default: {!r}'.format(dirdefault)
+    )
     return p.parse_args(args)
 
 
